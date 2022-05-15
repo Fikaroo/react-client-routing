888e14688ef1c5e6ea8d047a24008c231778cefe
Fikaroo <fuadbayramv@gmail.com>
2022-05-15 19:57:11 +0400

    Algoritmika Client Routing Task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./Pages/About";
+import Portfolio from "./Pages/Portfolio";
+import Contacts from "./Pages/Contacts";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar justify-between flex w-2/4 m-auto">
+        <Link className="navbar-brand" to="/#">
+          Home
+        </Link>
+
+        <Link className="nav-link" to="/portfolio">
+          Portfolio
+        </Link>
+
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+
+        <Link className="nav-link" to="/contacts">
+          Contacts
+        </Link>
+      </nav>
+      
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
     </div>
   );
 }
